9f362216d156db026109ac3951c32f9e367cc66d
mohiminulsemon <mohiminulsemon80@gmail.com>
2024-06-15 15:27:57 +0600

    importing middlewares in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,23 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
-// import notFound from "./app/middlewares/notFound";
-// import globalErrorHandler from "./app/middlewares/globalErrorHandler";
-// import router from "./app/routes";
+import router from "./app/routes";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
 
-// app.use("/api", router);
+app.use("/api", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Sports Facility Booking Platform server is up and running");
 });
 
-// app.use(notFound);
+app.use(notFound);
 
-// app.use(globalErrorHandler);
+app.use(globalErrorHandler);
 
 export default app;
