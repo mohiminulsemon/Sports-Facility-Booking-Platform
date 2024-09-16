448060f0c856c1d982d82260dd892fc2e71c0d43
mohiminulsemon <mohiminulsemon80@gmail.com>
2024-09-16 23:58:47 +0600

    updated user routes

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,19 +2,41 @@ import express from "express";
 import { UserControllers } from "./user.controller";
 import validateRequest from "../../middlewares/validateRequest";
 import { loginValidationSchema, userValidationSchema } from "./user.validation";
+import authenticate from "../../middlewares/auth.middleware";
+import { StatusCodes } from "http-status-codes";
 
 const router = express.Router();
+
+
 
 router.post(
   "/signup",
   validateRequest(userValidationSchema),
-  UserControllers.userSignUp,
+  UserControllers.userSignUp
 );
 
 router.post(
   "/login",
   validateRequest(loginValidationSchema),
-  UserControllers.loginUser,
+  UserControllers.loginUser
 );
 
+router.post("/logout", authenticate, (req, res) => {
+  res.status(StatusCodes.OK).json({
+    success: true,
+    statusCode: StatusCodes.OK,
+    message: "User logged out successfully",
+  });
+});
+
+
+router.get("/profile", authenticate, (req, res) => {
+  res.status(StatusCodes.OK).json({
+    success: true,
+    statusCode: StatusCodes.OK,
+    data: req.user, 
+  });
+});
+
 export const UserRoutes = router;
+
